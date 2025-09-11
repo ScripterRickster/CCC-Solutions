88463ea9da5e2a5f557095e1cb80eaf23ccc6767
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-09-11 19:16:07 -0400

    Create CCC '20 J1.cpp

---
+++ C++ - Solutions/CCC '20 J1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+	int s,m,l;
+    cin >> s >> m >> l;
+
+    (s + (2*m) + (3*l)) > 9 ? cout << "happy" : cout << "sad";
+    
+}
